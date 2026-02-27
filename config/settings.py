5657fec2bc5ceb5b474874e58ab1526971ef7eb6
Soyibjon <soyibjon12ss@gmail.com>
2026-02-27 19:44:08 +0500

    React uchun 8080 portiga ruxsat berildi

--- config/settings.py
+++ config/settings.py
@@ -210,6 +210,8 @@ DJOSER = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:8080",  # ← shu qatorni qo'shing
+
 ]
 
 # CORS_ALLOW_ALL_ORIGINS = True
