7958138efad798c53461ee3ca88fd6eee55bbfea
Soyibjon <soyibjon12ss@gmail.com>
2026-02-28 20:01:58 +0500

    Celerey name update

--- config/celery.py
+++ config/celery.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('ecommerce')
+app = Celery('shops_platform')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
